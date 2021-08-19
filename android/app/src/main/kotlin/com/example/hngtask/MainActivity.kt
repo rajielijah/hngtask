085348f3d386fb2c31826e5f73227b3f93110b5d
Raji Elijah <rajielijah@gmail.com>
2021-08-19 22:31:43 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/hngtask/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hngtask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+  import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Display User Input',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: UserInput(),
+    );
+  }
+}
+
+class UserInput extends StatefulWidget {
+  @override
+  _UserInputState createState() => _UserInputState();
+}
+
+class _UserInputState extends State<UserInput> {
+   final myController = TextEditingController();
+  @override
+   void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( 
+        title: Text('Display User Input'),
+      ),
+          body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: TextField(
+                controller: myController,
+                 decoration: InputDecoration(  
+      border: InputBorder.none,  
+      labelText: 'Enter Name',  
+      hintText: 'Enter Your Name'  
+  ),  
+              ),
+            ),
+            RaisedButton(onPressed: (){
+              showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                // Retrieve the text the that user has entered by using the
+                // TextEditingController.
+                content: Text(myController.text),
+              );
+            },
+          );
+            }, child: Text('Press to view your name'))
+          ],
+        ),
+      ),
+    );
+  }
+}
